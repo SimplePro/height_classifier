2f050175ef936f43cbe3685f41633ef1e141e91a
donghwan Kim <66504341+SimplePro@users.noreply.github.com>
2020-12-24 21:19:29 +0900

    added height pred module file

---
+++ height_pred.py
@@ -0,0 +1,13 @@
+import joblib
+import numpy as np
+
+model = joblib.load("./height_classifier.pkl")
+
+
+class Height():
+    def __init__(self, height=160, weight=80):
+        self.height = height[1:]
+        self.weight = weight
+
+    def predict(self):
+        return model.predict(np.array([self.height, self.weight]).reshape(1, -1))[0]
